c96893f7a087d39c3e6051f424ba6c1bddd31659
MANOJKUMAR5712 <kmanojkumar1098@gmail.com>
2023-12-11 16:54:16 +0530

    Day 2 : Project was created and Executed

--- 4_expressjs/index.js
+++ 4_expressjs/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 3000
  app.get('/', (req,res) => res.send('Hello World!'))
- app.get('/manojkumar',(req,res) => res.send('Name : Manojkumar  Roll no : 7376222IT193'));
+ app.get('/praveen',(req,res) => res.send('Name : Manojkumar  Roll no : 7376222IT193'));
 
  app.listen(PORT, () => console.log(
     `Example app listening at http://localhost:${PORT}`
